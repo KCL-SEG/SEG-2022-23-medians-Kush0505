4991ee6596896c981a96670c1d3c1767fee370c4
Kushaagra Kapoor <kushaagrakapoor@gmail.com>
2022-10-01 14:26:39 +0100

    Add median function

--- median.py
+++ median.py
@@ -2,13 +2,14 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def median(numberList):
-    if(numbers.size() % 2 == 0):
-        index1 = numbers.size()/2
-        index2 = index1 +1
+    if(len(numbers) % 2 == 0):
+        print(len(numbers))
+        index1 = int(len(numbers)/2)
+        index2 = int(index1-1)
         median = numbers[index1] + numbers[index2]
         median = median/2
     else:
-        index1 = numbers.size() +1
+        index1 = int(len(numbers) -1)
         median = numbers[index1]
     return median
 
@@ -16,7 +17,7 @@ while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
-        median(numbers)
+        print(median(numbers))
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
