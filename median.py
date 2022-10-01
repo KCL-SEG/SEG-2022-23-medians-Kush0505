79051172a8f3eaceeacb1885e37a609fd3b521e8
Kushaagra Kapoor <kushaagrakapoor@gmail.com>
2022-10-01 15:02:09 +0100

    add sorting and correct index variables

--- median.py
+++ median.py
@@ -2,24 +2,42 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def median(numberList):
-    if(len(numbers) % 2 == 0):
+    for i in range(len(numberList)):
+        lowest = numberList[i]
+        for j in range(0,len(numberList)):
+            #print(i,j)
+            if numberList[j] > numberList[i]:
+                c = numberList[i]
+                numberList[i] = numberList[j]
+                numberList[j] = c
+    #print(numberList)
+    index1 = 0
+    length = len(numberList)
+    #print("len" , length)
+    if(length % 2 == 0):
         #print(len(numbers))
-        index1 = int(len(numbers)/2)
-        index2 = int(index1-1)
-        median = numbers[index1] + numbers[index2]
+        index0 = length/2
+        index1 = int(index0)
+        index2 = index1-1
+        #print(index1)
+        median = numberList[index1] + numberList[index2]
         median = median/2
     else:
-        index1 = int(len(numbers) -1)
-        median = numbers[index1]
+        index0 = length/2
+        #print(index0)
+        index1 = int(index0)
+        #print(index1)
+        median = numberList[index1]
     return median
 
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
+        
         print(median(numbers))
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+#print(numbers)
